bd62d49e8829d816f00adbd5b2374df93a219a62
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2024-04-09 13:51:47 -0300

    triangulos

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Triangulos triangulo = new Triangulos();
+
+            Console.WriteLine("Verifivar o tipo de triangulo");
+            Console.Write("Informe o primeiro Valor:");
+            triangulo.x = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Informe o primeiro Valor:");
+            triangulo.y = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Informe o primeiro Valor:");
+            triangulo.z = Convert.ToDecimal(Console.ReadLine());
+
+            if (triangulo.CalcularTriangulo() < triangulo.z) 
+            {
+                if (triangulo.x == triangulo.z && triangulo.y == triangulo.z && triangulo.x == triangulo.y) 
+                {
+                    Console.WriteLine("Triângulo equilátero");
+                }
+                else if (triangulo.x != triangulo.z && triangulo.y != triangulo.z && triangulo.x != triangulo.y)
+                {
+                    Console.WriteLine("Triângulo Escaleno");
+                }
+                else Console.WriteLine("Triângulo Isósceles");
+
+            }
+            else Console.WriteLine("Triangulo invalido");
+
+
+
         }
     }
 }
---
+++ src/Triangulos.ConsoleApp/triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangulos.ConsoleApp
+{
+    public class Triangulos
+    {
+        public decimal x, y, z=0;
+
+        public decimal CalcularTriangulo()
+        {
+           decimal  soma = x + y;
+            return soma;
+        }
+    }
+}
